6da5730fd487897c5281c49c8ea188555785aef3
olucasjose <65680721+olucasjose@users.noreply.github.com>
2026-08-11 20:23:51 -0300

    feat: initialize Tauri backend with build pipelines and configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,18 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  clearScreen: false,
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: process.env.TAURI_DEV_HOST || false,
+    hmr: process.env.TAURI_DEV_HOST
+      ? {
+          protocol: 'ws',
+          host: process.env.TAURI_DEV_HOST,
+          port: 1421,
+        }
+      : undefined,
+  },
+  envPrefix: ['VITE_', 'TAURI_ENV_'],
 })
